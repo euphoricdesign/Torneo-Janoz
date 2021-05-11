5101db99dd32804a4a199711950d081399e0eba3
euphoricdesign <noahchukita@gmail.com>
2021-05-10 23:40:36 -0300

    correcting spelling mistakes

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -10,7 +10,7 @@ const FormSuccess = ({ uniqueCodeToShow }) => {
                 <div className="der">
                     <h1 className="font-weight-bold">ESTAS CADA VEZ MÁS CERCA DE ENFRENTARTE A LOS MEJORES</h1>
                     <hr className="hr-suc" style={{marginBottom: 50}} />
-                    <p className="first-p">El torneo iniciara el día 15 de Junio a las 15hs. Para continuar con tu registro debes ingesar a la página del torneo y colocar tu código único.</p>
+                    <p className="first-p">El torneo iniciará el día 15 de Junio a las 15hs. Para continuar con tu registro debes ingresar a la página del torneo y colocar tu código único.</p>
                     <p className="codigo">{uniqueCodeToShow}</p>
                     <div className="centrar">
                         <button
